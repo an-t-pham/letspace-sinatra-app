70d00b429d4b5f127268f9ba8e60209ea8f39f42
anpham148 <anpham148@gmail.com>
2020-10-31 08:16:39 +0000

    create all models + migrations

--- db/migrate/20201030063055_create_tenants.rb
+++ db/migrate/20201030063055_create_tenants.rb
@@ -2,7 +2,7 @@ class CreateTenants < ActiveRecord::Migration
   def change
     create_table :tenants do |t|
       t.string :name
-      t.string :profile
+      t.text :profile
       t.string :image_url
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20201031075242) do
 
   create_table "tenants", force: :cascade do |t|
     t.string   "name"
-    t.string   "profile"
+    t.text     "profile"
     t.string   "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
